bd202c5944119d0560804b49959961e7061b7c2c
MartinezN2014 <2014nmartinez@gmail.com>
2023-06-21 21:40:54 -0400

    Updated the script file to make a working password generator

--- script.js
+++ script.js
@@ -10,22 +10,56 @@ function writePassword() {
   // 3. generate password based on criteria
   // 4. display password
 
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numb = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "~"];
-  // var password = generatePassword();
+var numbers = "2134567890"
+var special = "!@#$%^&*()_+"
+var lower = "abcdefghijklmnopqrstuvwxyz"
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+var numb= numbers.split("")
+var specialChar = special.split("")
+var lowerCase = lower.split("")
+var upperCase = upper.split("")
+  var password = ""
   var passwordText = document.querySelector("#password");
 
-  var userChoice = window.prompt('How many characters would you like your password to contain?');
+  var userChoice = parseInt(prompt('How many characters would you like your password to contain?'));
   var userChoice2 = window.confirm('Click OK if you want to use lowercase characters');
   var userChoice3 = window.confirm('Click OK if you want to use uppercase characters');
   var userChoice4 = window.confirm('Click OK if you want to use numbers');
   var userChoice5 = window.confirm('Click OK if you want to use special characters');
-  passwordText.value = password;
-if (!userChoice) {
+    var selectedChar = []
+// console.log(userChoice);
+
+/*this ask the user how they would like their password to be built*/
+if (isNaN(userChoice) || userChoice < 8 || userChoice > 128 ) {
+    alert("Invalid Number");
   return;
+} 
+
+if (userChoice2) {
+    selectedChar = selectedChar.concat(lowerCase);
 }
+
+if (userChoice3) {
+  selectedChar = selectedChar.concat(upperCase); 
+}
+
+if (userChoice4) {
+  selectedChar = selectedChar.concat(numb); 
+}
+
+if (userChoice5) {
+  selectedChar = selectedChar.concat(specialChar);
+}
+/*this generates my password based on the if statement above*/
+console.log(selectedChar);
+for(var i = 0; i < userChoice; i++) {
+    var index = Math.floor(Math.random() * selectedChar.length);
+    var computerChoice = selectedChar[index];
+    console.log(computerChoice);
+    password += computerChoice;
+}
+console.log(password);
+passwordText.value = password;
 }
 
 // Add event listener to generate button
